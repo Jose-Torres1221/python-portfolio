bf6fd6966dea71e6480e55ae8ee8bd10096d303f
Jose-Torres1221 <72082170+Jose-Torres1221@users.noreply.github.com>
2022-12-02 00:28:32 -0600

    Create search_images.py

---
+++ python/search_images/search_images.py
@@ -0,0 +1,58 @@
+import search_text
+
+def read_lines(filename):
+    return search_text.read_lines(filename)
+
+def create_dictionary(lines):
+    del lines[0]
+    image_dict = {}
+    
+    for line in lines:
+        line = line.split(',')
+        tag_set = set()
+        for ele in line[2:]:
+            tag_set.add(ele)
+        image_dict[line[1], line[0]] = tag_set
+        
+    return image_dict
+
+def get_user_query():
+    query_tags = set()
+    query = input('Enter search query: ')
+    query = query.split(sep = ' ')
+    for ele in query:
+        query_tags.add(ele)
+    return query_tags
+
+def display_matches(query_tags, image_dict):
+    print('Results:')
+    count = 0
+    
+    # fix!
+    for key in image_dict:
+        if query_tags <= image_dict[key]:
+            count += 1
+            print(f'Image {key[0]}: {key[1]}')
+    print(f'{count} matches found.\n')
+
+def main():
+    print('\nProblem: Image Search using Tags\n')
+    FILENAME = 'search_images.csv'
+    cont = 'YES'
+    
+    # fill in
+    lines = read_lines(FILENAME)
+    image_dict = create_dictionary(lines)
+    while cont == 'YES':
+        
+        query_tags = get_user_query()
+        display_matches(query_tags, image_dict)
+
+        
+        #Continuing the loop
+        cont = input('Do you want to search again? ')
+        cont = cont.upper()
+        
+    print('\nGoodbye!')
+   
+main()
