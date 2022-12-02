0e2100f59842806da98591fd8373306e671270a8
Jose-Torres1221 <72082170+Jose-Torres1221@users.noreply.github.com>
2022-12-01 23:28:44 -0600

    Create SimpleDivisionCalculator.py

---
+++ python/SimpleDivisionCalculator.py
@@ -0,0 +1,12 @@
+#Aquiring Numbers
+dividend = int(input('Enter dividend: '))
+divisor = int(input('Enter divisor: '))
+int(divisor)
+print('Thank you.')
+
+#Aquiring Quotient and Remainder
+Quotient = dividend // divisor
+Remainder = dividend % divisor
+
+#Printing Answer
+print(f'Here are your results: {Quotient} remainder {Remainder}.')
