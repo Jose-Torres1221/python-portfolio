d7fc11d4809fab8107bc6654af028d464d0fead5
Jose-Torres1221 <72082170+Jose-Torres1221@users.noreply.github.com>
2022-12-01 23:33:04 -0600

    Create MilesPerGallon.py

---
+++ python/MilesPerGallon.py
@@ -0,0 +1,10 @@
+#Aquring Data
+miles_Driven = float(input('How many miles driven: '))
+gas_Used = float(input('How many gallons of gas was used: '))
+print('Thank you!')
+
+#Calculating MPG
+MPG = miles_Driven / gas_Used
+
+#Print Result
+print(f'Your Miles Per Gallon (MPG) are {MPG}.')
